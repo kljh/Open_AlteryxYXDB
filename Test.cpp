829cfd85b371c2057c3c88bbbb4eced2c236b164
Claude Cochet <code@main2.fr>
2019-05-16 22:06:49 +0100

    YXDB to SQLite database file

--- Test.cpp
+++ Test.cpp
@@ -1,64 +1,8 @@
-
 #include "stdafx.h"
 #include "Open_AlteryxYXDB.h"
 #include <iostream>
 #include <fstream>
-#include <random>
-
-// only used for generating sample data
-SRC::AString EnglishNumber(int n);
-
-void WriteSampleFile(const wchar_t *pFile)
-{
-	// the RecordInfo structure defines a record for the YXDB file.
-	SRC::RecordInfo recordInfoOut;
-
-	// Before you can create a YXDB, you need to tell the RecordInfo what fields it will expect
-	// Use CreateFieldXML to properly format the XML that describes a field
-	recordInfoOut.AddField(SRC::RecordInfo::CreateFieldXml(L"Number", SRC::E_FT_Double));
-	recordInfoOut.AddField(SRC::RecordInfo::CreateFieldXml(L"English", SRC::E_FT_V_String, 256));
-
-	//Now that we have defined the fields, we can create the file.
-	Alteryx::OpenYXDB::Open_AlteryxYXDB fileOut;
-
-	fileOut.Create(pFile, recordInfoOut.GetRecordXmlMetaData());
-
-	// in order to add a record to the file, we need to create an empty record and then fill it in
-	SRC::SmartPointerRefObj<SRC::Record> pRec = recordInfoOut.CreateRecord();
-
-	// just creating some random #'s to have some data to put into the file.
-	std::mt19937 r;
-	for (unsigned x = 0; x<100; ++x)
-	{
-		// this is very important.  ALWAYS reset the record to clear out all the variable length data
-		// if you skip this step, it will appear to work, but the record will get larger and larger
-		// generating an extremely inefficient YXDB - and eventually running out of memory.
-		// this will not actually clear out the values though.  You still need to either call:
-		//		SetFromXXX(...)
-		//	or
-		//		SetNull(...)
-		// for every field in the record.
-		pRec->Reset();
-		int v = r();
-
-		// the recordInfo object contains an array for FieldBase objects.
-		// although the fields are strongly typed, the field wrapper will convert when needed
-		// which is to say it will accept setting the field as a different type than what it is writing
-
-		// in this case we are setting the value as an integer, but the field is actually a float.
-		// that is OK because the Field library will convert it for us.
-		recordInfoOut[0]->SetFromInt32(pRec.Get(), v);
-		recordInfoOut[1]->SetFromString(pRec.Get(), EnglishNumber(v));
-
-		// now that we have filled out the record, we can add it to the file
-		fileOut.AppendRecord(pRec->GetRecord());
-	}
-
-	// calling Close is actually optional, since the destructor will call it for you.
-	// but if you don't explicitly call it, it will not be able to throw exceptions if the final 
-	// write fails for any reason
-	fileOut.Close();
-}
+#include <sqlite3.h>
 
 void replace_all(std::string& str, const std::string& search_str, const std::string& replace_by_str) {
 	size_t pos = 0;
@@ -82,7 +26,7 @@ std::string rfc4180_csv_escape(const char *cstr) {
 	return "\"" + str + "\"";
 }
 
-void ReadSampleFile(const wchar_t *pFile, const wchar_t *pCsvOutFile)
+void ReadToCsvFile(const wchar_t *pFile, const wchar_t *pCsvOutFile)
 {
 	Alteryx::OpenYXDB::Open_AlteryxYXDB file;
 	file.Open(pFile);
@@ -141,78 +85,180 @@ void ReadSampleFile(const wchar_t *pFile, const wchar_t *pCsvOutFile)
 		out << "\n";
 	}
 }
+
+std::string ansi_sql_escape(const char *cstr) {
+	return rfc4180_csv_escape(cstr);
+}
+
+static int sqlite_exec_callback(void *NotUsed, int argc, char **argv, char **azColName) {
+    for (int i = 0; i<argc; i++) {
+      	printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	return 0;
+}
+
+int ReadToSQLiteFile(const wchar_t *pFile, const wchar_t *pSQLiteOutFile, const wchar_t *pSQLiteOutTable)
+{
+	Alteryx::OpenYXDB::Open_AlteryxYXDB file;
+	fprintf(stdout, "YXDB path: %S \n", pFile);
+	file.Open(pFile);
+	int numRec(file.GetNumRecords());
+	fprintf(stdout, "YXDB records: #%i\n", numRec);
+	
+
+	sqlite3 *db;
+	char *err = 0;
+	int rc = sqlite3_open16(pSQLiteOutFile, &db);
+	if (rc) {
+		fprintf(stderr, "SQLite open error: %s.\n", sqlite3_errmsg(db));
+		return 1;
+	}
+
+	std::wstring table_name_wstr(pSQLiteOutTable);
+	std::string  table_name(table_name_wstr.begin(), table_name_wstr.end());
+	
+	std::string drop_table_stmt, create_table_stmt, insert_stmt;
+	for (unsigned j = 0; j < file.m_recordInfo.NumFields(); ++j)
+	{
+		if (j != 0) 
+			create_table_stmt +=  ", ";
+
+		const SRC::FieldBase * pField = file.m_recordInfo[j];
+		create_table_stmt += ansi_sql_escape(SRC::ConvertToAString(pField->GetFieldName().c_str()));
+		
+		if (IsBinary(pField->m_ft))
+			create_table_stmt += " BLOB";
+		else if (IsFloat(pField->m_ft))
+			create_table_stmt += " REAL";
+		else if (IsBoolOrInteger(pField->m_ft))
+			create_table_stmt += " INTEGER";
+		else if (IsStringOrDate(pField->m_ft))
+			create_table_stmt += " TEXT";
+
+		insert_stmt += j==0 ? "?" : ", ?";
+	}
+	
+	drop_table_stmt = "DROP TABLE IF EXISTS " + table_name;
+	create_table_stmt = "CREATE TABLE " + table_name  +" ( " + create_table_stmt + " )";
+	insert_stmt = "INSERT INTO " + table_name + " VALUES ( " + insert_stmt + " )";
+
+	if (SQLITE_OK != (rc = sqlite3_exec(db, drop_table_stmt.c_str(), sqlite_exec_callback, 0, &err))) {
+		fprintf(stderr, "SQLite DROP error: %s.\n", err);
+		sqlite3_free(err);
+		return 1;
+	}
+	if (SQLITE_OK != (rc = sqlite3_exec(db, create_table_stmt.c_str(), sqlite_exec_callback, 0, &err))) {
+		fprintf(stderr, "SQL CREATE error: %s\n", err);
+		sqlite3_free(err);
+		return 1;
+	}
+	if (SQLITE_OK != (rc = sqlite3_exec(db, "BEGIN TRANSACTION", sqlite_exec_callback, 0, &err))) {
+		fprintf(stderr, "SQL BEGIN TRANSACTION error: %s\n", err);
+		sqlite3_free(err);
+		return 1;
+	}
+	
+	sqlite3_stmt *stmt = nullptr;
+	if (SQLITE_OK != (rc = sqlite3_prepare_v2(db, insert_stmt.c_str(), -1, &stmt, nullptr))) {
+		auto err = sqlite3_errmsg(db);
+		fprintf(stderr, "SQL prepare error %d: %s\n", rc, err);
+		return 1;
+	}
+
+	int iRec = 0;
+	while (const SRC::RecordData *pRec = file.ReadRecord())
+	{
+		iRec++;
+		if (iRec%25000==0)
+			fprintf(stdout, "record %i / %i.\n", iRec, numRec);
+
+
+		for (unsigned j = 0; j<file.m_recordInfo.NumFields(); ++j)
+		{
+			int pos = j + 1;
+
+			const SRC::FieldBase * pField = file.m_recordInfo[j];
+			if (IsBinary(pField->m_ft))
+			{
+				// binary fields are not implicitly convertable to strings
+				rc = sqlite3_bind_null(stmt, pos);
+			}
+			else if (IsBoolOrInteger(pField->m_ft))
+			{
+				__int64 i = pField->GetAsInt64(pRec).value;
+				rc = sqlite3_bind_int64(stmt, pos, i);
+			}
+			else if (IsFloat(pField->m_ft) || IsNumeric(pField->m_ft)) // FixedDecimal is not a Float
+			{
+				double d = pField->GetAsDouble(pRec).value;
+				rc = sqlite3_bind_double(stmt, pos, d);
+			}
+			else if (IsStringOrDate(pField->m_ft))
+			{
+				const wchar_t* s = pField->GetAsWString(pRec).value.pValue;
+				rc = sqlite3_bind_text16(stmt, pos, s, -1, SQLITE_TRANSIENT);
+			}
+			else
+			{
+				fprintf(stderr, "SQL bind unknown type.\n");
+				rc = sqlite3_bind_null(stmt, pos);
+			}
+
+			if (rc != SQLITE_OK) {
+				auto err = sqlite3_errmsg(db);
+				fprintf(stderr, "SQL bind error %d: %s\n", rc, err);
+				return 1;
+			}
+		}
+
+		rc = sqlite3_step(stmt);
+		if (SQLITE_DONE != rc) {
+			auto err = sqlite3_errmsg(db);
+			printf("SQLite step error %d: %s\n", rc, err);
+			return 1;
+		}
+		if (SQLITE_OK != (rc = sqlite3_reset(stmt))) {
+			auto err = sqlite3_errmsg(db);
+			fprintf(stderr, "SQL prepare error %d: %s\n", rc, err);
+			return 1;
+		}
+	}
+
+	if (SQLITE_OK != (rc = sqlite3_exec(db, "COMMIT TRANSACTION", sqlite_exec_callback, 0, &err))) {
+		fprintf(stderr, "SQL COMMIT TRANSACTION error: %s\n", err);
+		sqlite3_free(err);
+		return 1;
+	}
+
+	if (stmt) sqlite3_finalize(stmt);
+	sqlite3_close(db);
+	return 0;
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	// most of the functions in this library can throw class Error if something goes wrong
 	try
 	{
-		if (argc > 2)
+		if (argc > 3)
 		{
-			ReadSampleFile(argv[1], argv[2]);
+			ReadToSQLiteFile(argv[1], argv[2], argv[3]);
+		} 
+		else if (argc > 2)
+		{
+			ReadToCsvFile(argv[1], argv[2]);
 		}
 		else
 		{
 			std::wstring wexe(argv[0]);
 			std::string  exe(wexe.begin(), wexe.end());
-			std::cout << "Usage: " << exe << " <yxdb input file>  <csv output file>.\n";
+			std::cout << "Usage: " << exe << " <yxdb input file> <.csv or .db/.sqlite output file> <sqlite table name>\n";
 		}
-
-		/*
-		WriteSampleFile(L"temp.yxdb");
-		ReadSampleFile(L"temp.yxdb");
-		*/
 	}
 	catch (SRC::Error e)
 	{
-		std::cout << SRC::ConvertToAString(e.GetErrorDescription()) << "\n";
+		std::cerr << SRC::ConvertToAString(e.GetErrorDescription()) << "\n";
 	}
 	return 0;
-}
-
-// only used for generating sample data
-SRC::AString EnglishNumber(int n)
-{
-	const static int numbers[] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90,100,1000,1000000,1000000000 };
-	const static SRC::AString english[] = { "zero", "one", "two", "three", "four", "five", "six","seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen","fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty","thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety","hundred", "thousand", "million", "billion" };
-
-	SRC::AString returnString;
-
-	if (n<0)
-	{
-		returnString = "negative";
-		n = -n;
-	}
-
-	for (int i = sizeof(numbers) / sizeof(*numbers) - 1; i >= 0; i--)
-	{
-		if (n == numbers[i])
-		{
-			if (!returnString.empty())
-				returnString += " ";
-			if (n >= 100)
-			{
-				returnString += english[1];
-				returnString += " ";
-			}
-
-			returnString += english[i];
-			break;
-		}
-		else if (n>numbers[i])
-		{
-			if (!returnString.empty())
-				returnString += " ";
-
-			if (n >= 100)
-			{
-				returnString += EnglishNumber(n / numbers[i]);
-				returnString += " ";
-			}
-
-			returnString += english[i];
-			n -= (n / numbers[i]) * numbers[i];
-		}
-	}
-
-	return returnString;
 }
